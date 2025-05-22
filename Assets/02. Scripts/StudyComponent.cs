9ce9816692d5ef018cfa8021887235e7a7a6a0ea
andancho <syw921023@gmail.com>
2025-05-22 16:33:36 +0900

    2025.05.22 수업내용
     - Destroy 관련
    Destroy(오브젝트) 메서드는 해당 오브젝트를 제거하는 기능
    OnDestroy 메서드는 이 스크립트가 달려있는 오브젝트를 제거했을 때 실행되는 메서드
    
     - Component
    AddComponent : 오브젝트에 컴포넌트를 추가하는 기능
    GetComponent : 오브젝트에 이미 존재하는 컴포넌트에 접근하는 기능
    
     - Input System
    게임에 널리 사용되는 이동에 관한 정의를 유니티 자체적으로 규정해놓은 시스템.
    이 시스템을 사용하면 캐릭터의 이동조작을 간단하게 구현할 수 있다
    float h = Input.GetAxis("Horizontal");
    h라는 소수타입 변수에 수평좌표 조작 값 할당
    Input.GetAxis("Vertical)";
    v라는 소수타입 변수에 수직좌표 조작 값 할당

--- Assets/02. Scripts/StudyComponent.cs
+++ Assets/02. Scripts/StudyComponent.cs
@@ -4,15 +4,26 @@ public class StudyComponent : MonoBehaviour
 {
     public GameObject obj;
 
-    public string changeName;
+    public Mesh msh;
+    public Material mat;
 
-    private void Start()
+    void Start()
     {
-        obj = GameObject.Find("Cube");
-
-        changeName = "NewName";
-        obj.name = changeName;
-        
+        CreatCube("Cube");
     }
 
+    public void CreatCube(string name)
+    {
+ 
+        obj = new GameObject(name);
+
+        obj.AddComponent<MeshFilter>();
+        obj.GetComponent<MeshFilter>().mesh = msh;
+
+        obj.AddComponent<MeshRenderer>();
+        obj.GetComponent<MeshRenderer>().material = mat;
+
+        obj.AddComponent<BoxCollider>();
+
+    }
 }
--- Assets/02. Scripts/StudyLog.cs
+++ Assets/02. Scripts/StudyLog.cs
@@ -13,31 +13,41 @@ public class StudyLog : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            moveSpeed = 1f;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            //transform.position += Vector3.forward * moveSpeed * Time.deltaTime; //z축 값 변경이라 시야에 따라 방향이 바뀌는 3d에 적합하지 않음
-            transform.position += transform.forward * moveSpeed * Time.deltaTime; 
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            this.transform.Rotate(0f, -7f*moveSpeed * Time.deltaTime, 0f);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += -transform.forward * moveSpeed * 0.5f * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            this.transform.Rotate(0f, 7f*moveSpeed * Time.deltaTime, 0f);
-        }
+        //Input System (Old - Legacy)
+        //입력값에 대한 약속된 시스템
+        //이동 WASD or 화살표
+        //점프 Space
+        //if (Input.GetKey(KeyCode.LeftShift))
+        //{
+        //    moveSpeed = 1f;
+        //}
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    //transform.position += Vector3.forward * moveSpeed * Time.deltaTime; //z축 값 변경이라 시야에 따라 방향이 바뀌는 3d에 적합하지 않음
+        //    transform.position += transform.forward * moveSpeed * Time.deltaTime; 
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    this.transform.Rotate(0f, -7f*moveSpeed * Time.deltaTime, 0f);
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.position += -transform.forward * moveSpeed * 0.5f * Time.deltaTime;
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    this.transform.Rotate(0f, 7f*moveSpeed * Time.deltaTime, 0f);
+        //}
 
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        Debug.Log($"현재 입력 : {dir}");
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
 
     }
 }
